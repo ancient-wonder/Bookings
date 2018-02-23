efcfcb3330fd5f64d4d3523c901264deef729f8b
jordynmcginnis <jordynm4@gmail.com>
2018-02-23 15:51:17 -0800

    working on server sending back reponse from database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
-const Booking = require('../database');
+const db = require('../database/index.js');
 let app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json());
+app.use(cors());
 
 let port = 1128;
+
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
+
+app.get('/bookings/:id', function (req, res) {
+
+  res.send('hiiiii');
+  //want to make a request to database getting document at id...
+  //send back document that matches that id...
+
+});
+
+
+
 
