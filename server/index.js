79aeec37b6c1755da488137e14aed521ed9565b9
ysera11 <hanyuzhu94@gmail.com>
2018-03-20 19:47:23 -0700

    refactor code to be more inline with style guide

--- server/index.js
+++ server/index.js
@@ -1,29 +1,25 @@
 const express = require('express');
-let db = require('../database/');
-let app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-app.use(express.static(__dirname + '/../client/dist'));
+const db = require('../database/mongoSchema');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyParser.json());
 app.use(cors());
-let port = 3002;
+const port = 3002;
 
-app.get('/api/bookings/:id', function (req, res) {
-  let cb = function (data) {
-    res.send(data);
-  }
-  db.find(req.params.id, cb);
+app.get('/api/bookings/:id', (req, res) => {
+  db.find(req.params.id)
+    .then(data => res.send(data));
 });
 
-app.get('/*', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '/../client/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
-
-app.listen(port, function() {
-  console.log(`listening on port ${port}`);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../client/index.html'), (err) => {
+    if (err) { res.status(500).send(err); }
+  });
 });
+
+app.listen(port, () => console.log(`listening on port ${port}`));
