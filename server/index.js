88a7b69cebad0ef78c45803bf11dabe64bd1514d
ysera11 <hanyuzhu94@gmail.com>
2018-03-23 14:50:58 -0700

    improve artillery config to test for randomized id

--- server/index.js
+++ server/index.js
@@ -1,19 +1,40 @@
 require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
+const redis = require('redis');
 const cors = require('cors');
 const db = require('../database/mongoSchema');
 
 const app = express();
+const client = redis.createClient();
+
+const cache = (req, res, next) => {
+  const id = req.params.id;
+  client.get(id, (err, data) => {
+    if (err) { throw err; }
+    if (data !== null) {
+      const result = JSON.parse(data);
+      res.send(result);
+    } else {
+      next();
+    }
+  });
+};
 
 app.use(express.static(`${__dirname}/../client/dist`));
 app.use(bodyParser.json());
 app.use(cors());
 const port = 3002;
 
-app.get('/api/bookings/:id', (req, res) => {
-  db.find(req.params.id)
-    .then(data => res.send(data));
+app.get('/api/bookings/:id', cache, async (req, res) => {
+  try {
+    const data = await db.find(req.params.id);
+    client.setex(req.params.id, 180, JSON.stringify(data));
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
